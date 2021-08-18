448b3714348dbb69625a2395ce912632f42484c8
nhoffudel <nicktwo505@gmail.com>
2021-08-18 11:07:12 -0400

    typescript exercises

--- rectangle.js
+++ rectangle.js
@@ -1,7 +1,5 @@
 function Rectangle(a, b) {
-    console.log("Length = " + a);
-    console.log("width = " + b);
-    console.log("Area = " + (a * b));
-    console.log("Perimeter = " + (2 * (a + b)));
+    var rectangle = { length: a, width: b, area: a * b, perimeter: 2 * (a + b) };
+    return rectangle;
 }
 console.log(Rectangle(4, 5));
--- rectangle.ts
+++ rectangle.ts
@@ -1,7 +1,5 @@
-function Rectangle(a: number, b: number): void {
-    console.log("Length = " + a);
-    console.log("width = " + b);
-    console.log("Area = " + (a*b));
-    console.log("Perimeter = " + (2 * (a + b)));
+function Rectangle(a: number, b: number): any {
+    const rectangle = {length : a, width : b, area : a*b, perimeter : 2 * (a+b)};
+    return rectangle;
 }
 console.log(Rectangle(4,5));
